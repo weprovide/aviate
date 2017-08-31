9656f0a10e225169d9189e591565dd08139fe133
Tim Neutkens <timneutkens@me.com>
2017-08-31 20:53:05 +0200

    clean up package

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const nodeVersion = require('node-version')
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 8) {
-  console.error('Error! wepack requires at least version 8 of Node. Please upgrade!')
+  console.error('Error! Aviate requires at least version 8 of Node. Please upgrade!')
   process.exit(1)
 }
 
